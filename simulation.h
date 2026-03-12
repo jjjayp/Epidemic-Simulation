b4e8bbb54a82f0408a1d1ccbb90bc3e95b5707f2
Jay Patel <jayp3999@gmail.com>
2026-03-12 00:52:33 -0400

    Update simulation.h

--- simulation.h
+++ simulation.h
@@ -9,11 +9,3 @@ int summary(THost hosts[], int m);
 int one_round(THost *hosts, int m, node **p_arr, int n_arr, int k, int T);
 
 #endif // SIMULATION_H
-
-
-// ===================== src/host.c =====================
-#include "host.h"
-
-int idx(int x, int y, int k) {
-    return x + k + (y + k) * (2 * k + 1);
-}
