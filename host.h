6f84660517eca9b6a5f54841767f6215afdb743b
Jay Patel <jayp3999@gmail.com>
2026-03-12 00:51:29 -0400

    Update host.h

---
+++ host.h
@@ -0,0 +1,13 @@
+#ifndef HOST_H
+#define HOST_H
+
+typedef enum TYPE { S, I, R } TYPE;
+
+typedef struct Host {
+    int id;
+    int x, y;
+    int t;
+    TYPE type;
+} THost;
+
+int idx(int x, int y, int k);
